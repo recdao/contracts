20ad303da7dc4874b2b4de90caa3e04fd1b54316
Carl Larson <cslarson@gmail.com>
2018-03-15 22:24:06 +0000

    work on ContentDAO prediction market contract

--- contracts/ContentDAO.sol
+++ contracts/ContentDAO.sol
@@ -4,38 +4,64 @@ import "./Interfaces.sol";
 
 contract ContentDAO {
 
-    enum Vote                         { DOWN, UP }
-
-    struct Flip {
-      mapping(address => uint)      stakes;
-    }
-
     struct Post {
-      Flip[]                        flips;
-      uint                          lastSigStakeAt;
+      uint                                           lastSigStakeAt;
+      uint                                           toFlip;
+      bool                                           liked;
+      mapping(bool => uint)                          totals;
+      mapping(bool => mapping(address => uint))      stakes;
     }
 
     IRegistry                       public registry;
     IToken                          public token;
+    uint                            public subunits;
     uint                            public MEMBER_MIN_KARMA = 500;
     uint                            public FLIP_PERCENT = 125;
-    uint                            public MAX_FLIPS = 10;
-    uint                            public SIG_STAKE = 200;
+    uint                            public ADJUDICATION_THRESHOLD = 10;
+    uint                            public SIG_STAKE = 200;                     // also serve as min stake?
+    uint                            public SIG_STAKE_DELAY = 43;                // delays end of staking for 10min if sig stake occurs
+    uint                            public STAKE_DURATION = 6000;               // ~24 hrs
 
     mapping(uint40 => Post)         public posts;
 
-    function ContentDAO (address _registry, address _token, uint _memberMinKarma) {
+    function ContentDAO (address _registry, address _token) {
         registry = IRegistry(_registry);
         token = IToken(_token);
-        MEMBER_MIN_KARMA = _memberMinKarma;
+        subunits = 10 ** uint(token.decimals());
     }
 
-    function stake(uint40 _id, Vote _vote, uint _amount) public {
-      // is open for staking (not finished or in adjudication)
+    function stake(uint40 _id, bool _vote, uint _amount) public {
+      Post storage post = posts[_id];
+      // TODO require is open for staking (not finished or in adjudication)
+
+      // stake needs to support side opposite to currently winning
+      require( _vote != post.liked );
+      // trim _amount
+      if(_amount > post.toFlip)
+          _amount = post.toFlip;
+
+      require( token.transferFrom(msg.sender, this, _amount) );
+
+      post.stakes[_vote][msg.sender] += _amount;
+      if(_amount == post.toFlip) flip(post);
+      if(_amount >= SIG_STAKE*subunits) post.lastSigStakeAt = block.number;
     }
 
-    function vote(uint40 _id, Vote _vote) public {
+    function flip(Post _post) internal {
+        post.liked = !post.liked;
+        post.toFlip = FLIP_PERCENT * post.toFlip / 100;
+    }
+
+    function init(uint40 _id, uint _amount) public {
+      require( token.transferFrom(msg.sender, this, _amount) );
+      Post memory post;
+      post.totals[true] += _amount;
+      post.lastSigStakeAt = block.number;
+    }
+
+    function vote(uint40 _id, bool _vote) public {
         require( isMember(msg.sender) );
+        // TODO - all
     }
 
     function isMember(address _address) public view returns(bool) {
